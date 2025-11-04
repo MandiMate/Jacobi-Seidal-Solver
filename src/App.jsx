be73b36470bc74e000dc1e2ef47ec8fd068c67a2
MandiMate <138322439+Naveed786Ahmed@users.noreply.github.com>
2025-11-04 23:43:31 +0500

    Create Solver

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import JacobiGaussSolver from './JacobiGaussSolver.jsx'
+
+const App = () => {
+  return (
+    <>
+      <JacobiGaussSolver />
+    </>
+
+  )
+}
+
+export default App
